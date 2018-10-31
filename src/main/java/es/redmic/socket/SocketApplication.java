08ebb1a9a6e5138105e5d0177e2a20d9c48d4f89
Noel Alonso <noel@redmic.es>
2018-10-31 11:36:42 +0000

    Añade tag a métricas de prometheus

--- src/main/java/es/redmic/socket/SocketApplication.java
+++ src/main/java/es/redmic/socket/SocketApplication.java
@@ -1,14 +1,27 @@
 package es.redmic.socket;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+
+import io.micrometer.core.instrument.MeterRegistry;
 
 @SpringBootApplication
 @ComponentScan({ "es.redmic.socket", "es.redmic.brokerlib" })
 public class SocketApplication {
 
+	@Value("${info.microservice.name}")
+	String microserviceName;
+
 	public static void main(String[] args) {
 		SpringApplication.run(SocketApplication.class, args);
+	}
+
+	@Bean
+	MeterRegistryCustomizer<MeterRegistry> metricsCommonTags() {
+		return registry -> registry.config().commonTags("application", microserviceName);
 	}
 }
